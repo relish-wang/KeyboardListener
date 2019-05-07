576775c3ee13b8dbc5797c955da1485fb868d2c7
relish-wang <relish.wang@gmail.com>
2019-05-07 17:33:51 +0800

    feat: 完成.

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 # KeyboardListener
 
-从react-native源码中抽离出来的用于监听Android软键盘**弹出**与**收回**事件的相关代码。
+**关于作者**
+
+> 景三，程序员，主要从事Android平台基础架构方面的工作，欢迎交流技术方面的问题，可以去我的[Github](https://github.com/relish-wang)提issue或者发邮件至relish.wang@gmail.com与我交流。
 
 [TOC]
 
+## 零、项目简介
 
-## 一、使用方法
+从[facebook/react-native](https://github.com/facebook/react-native)源码中抽离出来的用于**监听Android软键盘弹出与收回事件**的相关代码。
+
+**扫描二维码下载Demo**
+
+![下载Demo的二维码](./art/keyboardlistener-demo-qrcode.png)
+
+<center><strong>KeyboardListener-Demo.apk</strong></center>
+
+## 一、效果演示
+
+![显示底部NavigationBar](./art/with_navigation_bar.gif)
+
+<center><strong>图1 显示底部NavigationBar的情况</strong><br>(注意“屏幕可用高度”的数值)</center>
+
+![显示底部NavigationBar](./art/without_navigation_bar.gif)
+
+<center><strong>图2 隐藏底部NavigationBar的情况</strong><br>(注意“屏幕可用高度”的数值)</center>
+
+## 二、使用方法
 
 可以直接把[DisplayMetricsHolder](./app/src/main/java/wang/relish/keyboard/util/DisplayMetricsHolder.java)、[GlobalLayoutListener](./app/src/main/java/wang/relish/keyboard/util/GlobalLayoutListener.java)、[OnKeyboardChangedListener]((./app/src/main/java/wang/relish/keyboard/util/OnKeyboardChangedListener.java))三个文件直接拷贝到你的工程里使用。
 
@@ -27,9 +48,15 @@ rootView.getViewTreeObserver().addOnGlobalLayoutListener(
         }));
 ```
 
-## 二、致谢
+![显示底部NavigationBar](./art/desc.png)
 
-感谢facebook/react-native提供代码解决方案。
+## 三、更新日志
+
+[CHANGELOG.md](./CHANGELOG.md)
+
+## 四、致谢
+
+感谢[facebook/react-native](https://github.com/facebook/react-native)提供代码解决方案。
 
 本仓库使用的react-native相关代码:
 
--- app/src/main/java/wang/relish/keyboard/MainActivity.java
+++ app/src/main/java/wang/relish/keyboard/MainActivity.java
@@ -1,45 +1,122 @@
 package wang.relish.keyboard;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
 import wang.relish.keyboard.util.GlobalLayoutListener;
 import wang.relish.keyboard.util.OnKeyboardChangedListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnKeyboardChangedListener, View.OnClickListener {
 
     public static final String TAG = "KeyboardListener";
+    public static final String KEY_SHOW_NAVIGATION_BAR = "__showNavigationBar__";
 
 
+    private static void startActivityWithoutNavigationBar(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(KEY_SHOW_NAVIGATION_BAR, false);
+        context.startActivity(intent);
+    }
+
+    View mLlRoot;
+    /**
+     * 展示键盘改变相关数据
+     */
     TextView mTvResult;
+    /**
+     * 打开隐藏底部NavigationBar的MainActivity
+     */
+    TextView mBtnDismissBar;
+    /**
+     * 收起键盘
+     */
+    TextView mDismissKeyboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        boolean isShow = getIntent().getBooleanExtra(KEY_SHOW_NAVIGATION_BAR, true);
+        if (!isShow) {
+            hideBottomUIMenu();
+        }
+
+        mBtnDismissBar = findViewById(R.id.btn_dismiss_bar);
+        mBtnDismissBar.setOnClickListener(this);
+        mBtnDismissBar.setVisibility(isShow ? View.VISIBLE : View.GONE);
+
+        mDismissKeyboard = findViewById(R.id.btn_dismiss_keyboard);
+        mDismissKeyboard.setOnClickListener(this);
         mTvResult = findViewById(R.id.tv_result);
 
-        final View rootView = findViewById(R.id.ll_root);
-        rootView.getViewTreeObserver().addOnGlobalLayoutListener(
-                new GlobalLayoutListener(rootView, new OnKeyboardChangedListener() {
-                    @Override
-                    public void onChange(boolean isShow, int keyboardHeight, int screenWidth, int screenHeight) {
-                        StringBuilder sb = new StringBuilder();
-                        sb.append("isShow: ").append(isShow).append("\n")
-                                .append("keyboardHeight: ").append(keyboardHeight).append("\n")
-                                .append("screenWidth: ").append(screenWidth).append("\n")
-                                .append("screenHeight: ").append(screenHeight).append("\n");
-                        mTvResult.append(sb.toString() + "\n");
+        mLlRoot = findViewById(R.id.ll_root);
+        mLlRoot.getViewTreeObserver().addOnGlobalLayoutListener(new GlobalLayoutListener(mLlRoot, this));
+    }
 
-                        Log.d(TAG, "键盘是否展开: " + isShow);
-                        Log.d(TAG, "键盘高度(px): " + keyboardHeight);
-                        Log.d(TAG, "屏幕宽度(px): " + screenWidth);
-                        Log.d(TAG, "屏幕高度(px): " + screenHeight);
-                    }
-                }));
+    /**
+     * 键盘事件
+     *
+     * @param isShow         键盘是否展示
+     * @param keyboardHeight 键盘高度(当isShow为false时,keyboardHeight=0)
+     * @param screenWidth    屏幕宽度
+     * @param screenHeight   屏幕可用高度(不包含底部虚拟键盘NavigationBar), 即屏幕高度-键盘高度(keyboardHeight)
+     */
+    @Override
+    public void onChange(boolean isShow, int keyboardHeight, int screenWidth, int screenHeight) {
+        //noinspection StringBufferReplaceableByString
+        StringBuilder sb = new StringBuilder();
+        sb.append("键盘是否展开: ").append(isShow).append("\n")
+                .append("键盘高度(px): ").append(keyboardHeight).append("\n")
+                .append("屏幕宽度(px): ").append(screenWidth).append("\n")
+                .append("屏幕可用高度(px): ").append(screenHeight).append("\n");
+        mTvResult.append(sb.toString() + "\n");
+
+        Log.d(TAG, "键盘是否展开: " + isShow);
+        Log.d(TAG, "键盘高度(px): " + keyboardHeight);
+        Log.d(TAG, "屏幕宽度(px): " + screenWidth);
+        Log.d(TAG, "屏幕可用高度(px): " + screenHeight);
+    }
+
+
+    /**
+     * 隐藏虚拟按键，并且全屏
+     */
+    protected void hideBottomUIMenu() {
+
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            Window _window = getWindow();
+            WindowManager.LayoutParams params = _window.getAttributes();
+            params.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
+            _window.setAttributes(params);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_dismiss_bar:
+                MainActivity.startActivityWithoutNavigationBar(this);
+                break;
+            case R.id.btn_dismiss_keyboard:
+                InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (manager != null) {
+                    manager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                break;
+        }
     }
 }
